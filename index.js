7a0220320e93756e078fd97b38ca1e5642dbcaea
ayeshamahar12 <ayeshamahar007@gmail.com>
2024-07-04 11:21:58 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+var fristname="alisha"
+document.write(45-20);
+document.write(12*4);
+document.write(15+30);
+document.write(fristname);
+console.log("remisha");
+console.log("nasha");
+// alert("good moring");
+// alert("helloword")
+// alert("hello")
+connum=77;
+num=776
+num=33;
+console.log("77");
+let num="33";
+let num2="45";
+alert("num+num2")
+// console.log(um-num2);
+let name =import ("what is your name")
